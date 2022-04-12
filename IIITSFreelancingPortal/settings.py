2ac34eac34c31191e56290b54c63747ad6d9e9d3
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2022-04-12 21:19:16 +0530

    updated

--- IIITSFreelancingPortal/settings.py
+++ IIITSFreelancingPortal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2^adjpbk_eq&as+*5cys3d9htah)ajjx@@up444f^+7g-!#p47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
