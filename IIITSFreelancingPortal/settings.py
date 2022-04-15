be72e8c1bab8c73500f298ebb865c2e03032dda5
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2022-04-15 18:36:51 +0530

    modified ui and fixed logic

--- IIITSFreelancingPortal/settings.py
+++ IIITSFreelancingPortal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2^adjpbk_eq&as+*5cys3d9htah)ajjx@@up444f^+7g-!#p47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
