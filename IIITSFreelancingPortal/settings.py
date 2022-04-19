8e9125cac743cde18b89909069caa7b82d1dacbc
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2022-04-19 20:11:42 +0530

    made 13

--- IIITSFreelancingPortal/settings.py
+++ IIITSFreelancingPortal/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "PASSWORD": "login123@",
         "HOST": "supplier-server.database.windows.net",
         "PORT": "1433",
-        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server", 
+        "OPTIONS": {"driver": "ODBC Driver 13 for SQL Server", 
         },
     },
 }
